d27ed3f58ac2b940df3387587d28de6dfd525197
VErGy25~ <mahorkin2513@gmail.com>
2023-06-02 00:16:39 +0300

    Les8

---
+++ app/src/main/java/ru/mirea/makhorkin/mireaproject/MapActivity.java
@@ -0,0 +1,155 @@
+package ru.mirea.makhorkin.mireaproject;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.res.ResourcesCompat;
+import androidx.preference.PreferenceManager;
+
+import org.osmdroid.api.IMapController;
+import org.osmdroid.config.Configuration;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.Marker;
+import org.osmdroid.views.overlay.ScaleBarOverlay;
+import org.osmdroid.views.overlay.compass.CompassOverlay;
+import org.osmdroid.views.overlay.compass.InternalCompassOrientationProvider;
+import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
+
+import ru.mirea.makhorkin.mireaproject.databinding.ActivityMapBinding;
+
+public class MapActivity extends AppCompatActivity {
+    private MapView mapView = null;
+    private ActivityMapBinding binding;
+
+    MyLocationNewOverlay locationNewOverlay;
+
+    final private int REQUEST_CODE_ASK_PERMISSIONS = 222;
+
+    private double xloc;
+    private double yloc;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Configuration.getInstance().load(getApplicationContext(),
+                PreferenceManager.getDefaultSharedPreferences(getApplicationContext()));
+
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) !=
+                    PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{
+                                android.Manifest.permission.ACCESS_FINE_LOCATION},
+                        REQUEST_CODE_ASK_PERMISSIONS);
+                return;
+            }
+        }
+        binding = ActivityMapBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        mapView = binding.mapView;
+
+        mapView.setZoomRounding(true);
+        mapView.setMultiTouchControls(true);
+
+        IMapController mapController = mapView.getController();
+        mapController.setZoom(15.0);
+        //GeoPoint startPoint = new GeoPoint(55.794229, 37.700772);
+        //mapController.setCenter(startPoint);
+
+        locationNewOverlay = new MyLocationNewOverlay(new
+                GpsMyLocationProvider(getApplicationContext()), mapView);
+        locationNewOverlay.enableMyLocation();
+        mapView.getOverlays().add(this.locationNewOverlay);
+
+        CompassOverlay compassOverlay = new CompassOverlay(getApplicationContext(), new
+                InternalCompassOrientationProvider(getApplicationContext()), mapView);
+        compassOverlay.enableCompass();
+        mapView.getOverlays().add(compassOverlay);
+
+        final Context context = this.getApplicationContext();
+        final DisplayMetrics dm = context.getResources().getDisplayMetrics();
+        ScaleBarOverlay scaleBarOverlay = new ScaleBarOverlay(mapView);
+        scaleBarOverlay.setCentred(true);
+        scaleBarOverlay.setScaleBarOffset(dm.widthPixels / 2, 10);
+        mapView.getOverlays().add(scaleBarOverlay);
+
+        Bundle bundle = getIntent().getExtras();
+        String est = bundle.getString("Establishment");
+
+
+        String[] ests = getResources().getStringArray(R.array.est_names);
+        if(est.equals(ests[0])){
+            xloc = 55.7516;
+            yloc = 37.6178;
+        } else if (est.equals(ests[1])) {
+            xloc = 55.6700;
+            yloc = 37.4802;
+        } else if (est.equals(ests[2])) {
+            xloc = 55.7932;
+            yloc = 37.7008;
+        } else {
+            xloc = 55.7542;
+            yloc = 37.6204;
+        }
+        GeoPoint startPoint = new GeoPoint(xloc, yloc);
+        mapController.setCenter(startPoint);
+        Marker marker = new Marker(mapView);
+        marker.setPosition(new GeoPoint(xloc, yloc));
+        marker.setOnMarkerClickListener(new Marker.OnMarkerClickListener() {
+            public boolean onMarkerClick(Marker marker, MapView mapView) {
+                Toast.makeText(getApplicationContext(), est,
+                        Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+        mapView.getOverlays().add(marker);
+
+        marker.setIcon(ResourcesCompat.getDrawable(getResources(), org.osmdroid.library.R.drawable.osm_ic_follow_me_on, null));
+
+        marker.setTitle(est);
+
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        Configuration.getInstance().load(getApplicationContext(),
+                PreferenceManager.getDefaultSharedPreferences(getApplicationContext()));
+        if (mapView != null) {
+            mapView.onResume();
+        }
+    }
+    @Override
+    public void onPause() {
+        super.onPause();
+        Configuration.getInstance().save(getApplicationContext(),
+
+                PreferenceManager.getDefaultSharedPreferences(getApplicationContext()));
+
+        if (mapView != null) {
+            mapView.onPause();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CODE_ASK_PERMISSIONS:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                } else {
+                    Toast.makeText( this,"Разрешение" , Toast.LENGTH_SHORT)
+                            .show();
+                }
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
